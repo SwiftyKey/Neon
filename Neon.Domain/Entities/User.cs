5f2b966076a4b52dd3c2426e0fc947fa15ac4f8e
SwiftyKey <slipperyfish9@gmail.com>
2024-11-26 20:34:33 +0500

    изменил yfdpfybt

--- Neon.Domain/Entities/User.cs
+++ Neon.Domain/Entities/User.cs
@@ -8,7 +8,6 @@ public class User: BaseAuditableEntity
 	public required string HashPassword { get; set; }
 	public required bool IsAdmin { get; set; }
 
-	public ICollection<Product> Products { get; set; } = [];
 	public ICollection<History> History { get; set; } = [];
 	public ICollection<Order> Orders { get; set; } = [];
 }
